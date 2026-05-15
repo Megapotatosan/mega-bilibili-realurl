b2f264a3566c4977c7afed250160f2de59902337
JackyH <jackyhui@mail.megasan.xyz>
2026-05-15 23:56:11 +0800

    Use official tRPC fetch handler

--- server/routes/rpc/[...trpc].ts
+++ server/routes/rpc/[...trpc].ts
@@ -1,7 +1,12 @@
-import { defineTRPCEventHandler } from '@falcondev-oss/nitro-trpc-event-handler';
+import { eventHandler, toWebRequest } from 'h3';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '../../trpc/api-router';
 
-export default defineTRPCEventHandler({
-  router: appRouter,
-  createContext: async event => ({ event })
+export default eventHandler(event => {
+  return fetchRequestHandler({
+    endpoint: '/rpc',
+    req: toWebRequest(event),
+    router: appRouter,
+    createContext: async () => ({ event })
+  });
 });
